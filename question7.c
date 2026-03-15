613bd84491e99c36e6b61f0eeb37ffbf5c1eea49
Bhumika-code860 <bhumibansal785@gmail.com>
2026-03-15 19:08:38 +0530

    Create question7.c

---
+++ question7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{int a,b,c;
+printf("enter the first number:");
+scanf("%d",&a);
+printf("enter the second number:");
+scanf("%d",&b);
+printf("enter the third number:");
+scanf("%d",&c);
+if(a>b&&a>c)
+printf("first number is greatest");
+else if(b>a&&b>c)
+printf("second number is greatest");
+else
+printf("third number is greatest");
+return 0;
+}
