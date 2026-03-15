f87692e7b5c92d6897fd226e3779c4b5a6686cb1
Bhumika-code860 <bhumibansal785@gmail.com>
2026-03-15 18:48:44 +0530

    Create question4.c

---
+++ question4.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{float cent,fahren;
+printf("enter the temperature in centigrade:");
+scanf("%f",&cent);
+fahren=((9*cent)/5)+32;
+printf("temperature in fahrenheit is %f",fahren);
+return 0;}
